a741c62c6861a99b9c548c54176913ad1bdbbb87
Maxim <fedykmaxim@gmail.com>
2022-07-18 17:44:58 +0300

    Lessons 3

--- MyPlace/ViewController.swift
+++ MyPlace/ViewController.swift
@@ -7,13 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+    let restaurantNames = [
+                            "MacDonalds", "Stara Pidkova", "Stara Piceria",
+                           "Club Faynogo", "Fayne Misto", "Flamingo", "Kebab",
+                           "Mlyn", "Topilche", "Barbaresko", "Francua", "Ternozavr",
+                           "Aligator", "Abrykol", "Na Nebi"
+    ]
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
+        
     }
 
+
+    class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+        
+    }
 
 }
 
